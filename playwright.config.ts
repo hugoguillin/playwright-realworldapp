81e168781e08e7cd726d236ebc222dfdb897de47
hugoguillin <hugo5guillin@gmail.com>
2024-05-12 21:11:22 +0200

    Change setup auth to via api

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config'
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -18,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 4 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,59 +1,44 @@
-import { test as setup, expect } from '@playwright/test';
+import { test as setup } from '@playwright/test';
+import fs from 'fs/promises';
 
 const authFile = './.auth/user.json';
 
-setup('authenticate', async ({ page }) => {
-  // Perform authentication steps. Replace these actions with your own.
-  await page.goto('http://localhost:3000/#/login');
-  await page.getByTestId('email').fill('cypress@realworld.com');
-  await page.getByTestId('password').fill('cypress@realworld.com');
-  await page.getByTestId('login-button').click();
+setup('authenticate via api', async ({ request }) => {
+  const email = process.env.RWAPP_USER_EMAIL
+  const password = process.env.RWAPP_USER_PASSWORD
 
-  await expect(page.getByTestId('your-feed')).toBeVisible();
+  const response = await request.post('http://localhost:3001/api/users/login', {
+    data: {
+      "user": {
+        email,
+        password
+      }
+    }
+  })
 
-  // End of authentication steps.
-  await page.context().storageState({ path: authFile });
+  const res = await response.json()
+  const value = {
+    headers: {
+      Authorization: `Token ${res.user.token}`
+    },
+    isAuth: true,
+    loggedUser: res.user
+  }
+  const loggedUser = {
+    cookies: [],
+    origins: [
+      {
+        origin: "http://localhost:3000",
+        localStorage: [
+          {
+            name: "loggedUser",
+            value: JSON.stringify(value)
+          }
+        ]
+      }
+    ]
+  }
 
-  // Save auth token to environment variable so api requests can use it
-  const localStorage = await page.evaluate(() => localStorage.getItem('loggedUser'));
-  const loggedUser = JSON.parse(localStorage);
-  const token = loggedUser.headers.Authorization;
-  process.env.AUTH_TOKEN = token;
+  await fs.writeFile(authFile, JSON.stringify(loggedUser, null, 2));
+  process.env.AUTH_TOKEN = `Token ${res.user.token}`;
 });
-
-// setup('authenticate via api', async ({ request }) => {
-//   const response = await request.post('http://localhost:3001/api/users/login', {
-//     data: {
-//       "user": {
-//         "email": "cypress@realworld.com",
-//         "password": "cypress@realworld.com"
-//       }
-//     }
-//   })
-
-//   const res = await response.json()
-//   const loggedUser = {
-//     cookies: [],
-//     origins: [
-//       {
-//         origin: "http://localhost:3000",
-//         localStorage: [
-//           {
-//             name: "loggedUser",
-//             value: {
-//               headers: {
-//                 Authorization: `Token ${res.user.token}`
-//               },
-//               isAuth: true,
-//               loggedUser: res.user
-//             }
-//           }
-//         ]
-//       }
-//     ]
-//   }
-//   // await request.storageState({ path: authFile });
-//   await fs.writeFile(authFile, JSON.stringify(loggedUser, null, 2));
-//   const t = res.user.token;
-//   process.env.AUTH_TOKEN = `Token ${res.user.token}`;
-// });
