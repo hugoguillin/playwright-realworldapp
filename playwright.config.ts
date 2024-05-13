98324c7b0aa21ee6b47cdfed1b5466441a3fe8af
hugoguillin <hugo5guillin@gmail.com>
2024-05-13 14:03:56 +0200

    Set 2 workers to run tests on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : 2,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -22,7 +22,6 @@ export default defineConfig({
 
     /* Collect trace on test failure to debug easier. Available from report. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    video: 'retain-on-failure',
     extraHTTPHeaders: {
       'Authorization': `${process.env.AUTH_TOKEN}`
     }
