92c5c627efc4c953bf5526f5f18f5c14c6025de5
hugoguillin <hugo5guillin@gmail.com>
2024-05-13 20:20:39 +0200

    Remove unused import

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,8 +1,26 @@
 import { test as setup } from '@playwright/test';
-import { mkdir } from 'fs';
 import fs from 'fs/promises';
 
 const authFile = './.auth/user.json';
+
+// setup('authenticate', async ({ page }) => {
+//   // Perform authentication steps. Replace these actions with your own.
+//   await page.goto('http://localhost:3000/#/login');
+//   await page.getByTestId('email').fill('cypress@realworld.com');
+//   await page.getByTestId('password').fill('cypress@realworld.com');
+//   await page.getByTestId('login-button').click();
+
+//   await expect(page.getByTestId('your-feed')).toBeVisible();
+
+//   // End of authentication steps.
+//   await page.context().storageState({ path: authFile });
+
+//   // Save auth token to environment variable so api requests can use it
+//   const localStorage = await page.evaluate(() => localStorage.getItem('loggedUser'));
+//   const loggedUser = JSON.parse(localStorage);
+//   const token = loggedUser.headers.Authorization;
+//   process.env.AUTH_TOKEN = token;
+// });
 
 setup('authenticate via api', async ({ request }) => {
   const email = process.env.RWAPP_USER_EMAIL
